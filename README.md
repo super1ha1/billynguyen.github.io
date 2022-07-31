6aabe689619ebe30427982944124055fc69b9bf7
khue <khue0011@gmail.com>
2022-07-31 18:19:19 +0700

    change name

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # To deploy
 ```bash
-cd /Users/khue/workspace/super1ha1.github.io
+cd /Users/khue/workspace/billynguyen.github.io
 npm run build
 open .
 
 ```
-copy all files in build folder to super1ha1.github.io folder
+copy all files in build folder to billynguyen.github.io folder
 commit and push to master branch
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://super1ha1.github.io/precache-manifest.5d9b653792ec03160a39b449bc906c1d.js"
+  "https://billynguyen.github.io/precache-manifest.5d9b653792ec03160a39b449bc906c1d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://super1ha1.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://billynguyen.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
